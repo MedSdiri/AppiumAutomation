da83c7d8a2fd438f363369bf3e6e3c93cb63f74b
MedSdiri <sdiri.ad@gmail.com>
2021-04-19 15:36:08 -0500

    etsy test

---
+++ src/test/java/tests/EtsyTests.java
@@ -0,0 +1,67 @@
+package tests;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class EtsyTests {
+
+
+    AppiumDriver<AndroidElement> driver;
+    @Before
+    public void setup() throws MalformedURLException {
+        DesiredCapabilities dc = new DesiredCapabilities();
+        dc.setCapability(MobileCapabilityType.PLATFORM_NAME, Platform.ANDROID);
+        dc.setCapability(MobileCapabilityType.PLATFORM_VERSION, "7.0");
+        dc.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+        dc.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel_2");
+        dc.setCapability(MobileCapabilityType.APP, "https://cybertek-appium.s3.amazonaws.com/etsy.apk");
+
+        URL url = new URL("http://localhost:4723/wd/hub");
+        driver = new AndroidDriver<>(url, dc);
+    }
+
+
+    @Test
+    public void test() throws InterruptedException {
+        AndroidElement getStarted = driver.findElementById("com.etsy.android:id/btn_link");
+        getStarted.click();
+
+        //enter email
+        driver.findElementById("com.etsy.android:id/edit_username").sendKeys("areatha@uspeakw.com");
+        //enter password
+        driver.findElementById("com.etsy.android:id/edit_password").sendKeys("Cybertek2020");
+        //click sign in
+        Thread.sleep(3000);
+        driver.findElementById("com.etsy.android:id/button_signin").click();
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+    @After
+    public void tearDown(){
+        driver.closeApp();
+    }
+
+
+
+
+}
